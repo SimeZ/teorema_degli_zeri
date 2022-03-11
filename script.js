6093efad3ed24826c01e20c407b3e7b5d69aee98
Simone Zucaro <simonezucaro025@gmail.com>
2022-03-11 22:40:06 +0100

    Aggiunto il form per l'inserimento dei dati

---
+++ script.js
@@ -0,0 +1,20 @@
+"use strict";
+
+// HTML ELEMENTS
+const rangeDataEl = document.querySelector(".range-data");
+const functionDataEl = document.querySelector(".function-data");
+const ExecuteBtnEl = document.querySelector(".btn");
+
+// const params = {
+//   appName: "graphing",
+//   width: 800,
+//   height: 600,
+//   showToolBar: true,
+//   showAlgebraInput: true,
+//   showMenuBar: true,
+// };
+// const ggbApplet = new GGBApplet(params, true);
+// window.addEventListener("load", function () {
+//   ggbApplet.inject("ggb-element");
+//   ggbApplet.setVCoords("a", 1, 2);
+// });
